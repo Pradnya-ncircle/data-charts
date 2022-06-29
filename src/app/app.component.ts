60d8f1b9062fd7c9a773750afd00a7648720bf60
Pradnya Savakhande <pradnya.savakhande@GEMINI.NCIRCLETECH.COM>
2022-06-29 13:43:08 +0530

    added tree-view and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,29 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+
+
+interface assetNode{
+  name: string;
+  children? : assetNode[]
+}
+
+const treeData : assetNode[] = [
+  {
+    name : 'Asset 0',
+    children : []
+  },
+  {
+    name : 'Asset 1',
+    children : [
+      {name : 'Asset 2',children: []},
+      {name:'Asset 3',children : [
+        {name : 'Asset 4', children : []}
+      ]}
+    ]
+  }
+
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +31,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showFiller = true;
   title = 'data-charts';
+
+  treeControl = new NestedTreeControl<assetNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<assetNode>();
+
+  constructor(){
+    this.dataSource.data = treeData
+  }
+
+  hasChild = (_:number, node: assetNode)=> !!node.children && node.children.length>0;
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    NgChartsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
